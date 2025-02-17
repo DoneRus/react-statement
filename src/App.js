025a3aeb42f8eeb9d79ef0515a7a512da92e1b41
Bogdan Bunea <130248256+DoneRus@users.noreply.github.com>
2025-02-17 14:19:55 +0100

    Source Files from react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from "react";
+import './App.css';
+const data = require('./data.json');
+console.log(data);
+
+
+function App() {
+  return (
+   <><div><h1>kklslTEST"HERE"</h1>Iam grebbyyy</div><div>
+    </div></>
+  );
+  
+}
+
+export default App;
